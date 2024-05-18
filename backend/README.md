64281e99f8739dbb87e4a98d4c39ed979e7da87d
vincentiusvin <vincentius_vin@yahoo.com>
2024-05-18 11:48:01 +0700

    Documentation
    Add warning
    Warn against putting error messages in ResBody field
    
    Doc how to register functions

---
+++ backend/README.md
@@ -0,0 +1,58 @@
+# Getting Started
+
+Untuk buat Request Handler bisa ikutin template ini:
+
+```ts
+export const fn: RH = async function (req, res) {
+  res.json({ msg: "Halo" });
+};
+```
+
+RH diimport dulu. Ctrl+Space aja. Kalau gak muncul lokasinya ada di [sini](src/helpers/types.ts)
+
+Habis itu fungsinya di-register di [sini](src/routes.ts)
+Taruh di dalam `registerRoutes` dan juga di `_api`.
+
+```ts
+export function registerRoutes(app: Express) {
+  ...
+  app.get("/api/route-ikutin-rest", fn);
+  ...
+}
+```
+
+```ts
+type _api = {
+  ...
+  NamaKeyBebasTapiRapih: typeof fn;
+  ...
+};
+```
+
+# Type Safety
+
+RH itu generic, bisa kita masukin tipe data yang kita mau biar kodenya type-safe:
+
+```ts
+export const fn: RH<{
+  ResBody: { msg: string };
+}> = async function (req, res) {
+  return;
+};
+```
+
+Tipe data yang tersedia dan penjelasannya bisa dibaca di [sini](src/helpers/types.ts).
+
+# Error Handling
+
+Kalau ada error dan mau kirim error ke user, bisa langsung di throw aja. Jangan dibuat manual di objek res.
+Error bakal dikirim dalam bentuk `{msg: string}`, tapi ga perlu di-declare di type RH. bakal di-handle otomatis sama middleware.
+
+```ts
+export const fn: RH = async function (req, res) {
+  throw new ClientError("User salah input");
+};
+```
+
+Jenis error yang berbeda bakal nge-return HTTP status code yang berbeda.
+Lebih lengkapnya bisa dilihat di [sini](src/helpers/error.ts).
--- backend/src/helpers/types.ts
+++ backend/src/helpers/types.ts
@@ -1,10 +1,17 @@
 import { RequestHandler } from "express";
 
 type RequestHandlerOptions = {
-  Params?: Record<string, unknown>; // Param itu data kiriman yang ada di URL, http://website/api/items?page=1
-  ResBody?: Record<string, unknown> | Record<string, unknown>[]; // ResBody itu jawaban dari server
-  ReqBody?: Record<string, unknown>; // ReqBody itu data kiriman yang gaada di url
-  ReqQuery?: Record<string, unknown>; // ReqQuery itu data yang ada langsung di pathnya, http://website/api/user/1
+  // Param itu data kiriman yang ada di URL, http://website/api/items?page=1.
+  Params?: Record<string, unknown>;
+  // ResBody itu jawaban dari server. Declare untuk request yang sukses aja.
+  // Request gagal formatnya bakal pakai {msg: string} tapi ga usah dimasukkin ke sini.
+  // Kalau request sukses formatnya {msg: string} silahkan ditaruh disini.
+  ResBody?: Record<string, unknown> | Record<string, unknown>[];
+  // ReqBody itu data kiriman yang dtaruh di body, bukan di url.
+  ReqBody?: Record<string, unknown>;
+  // ReqQuery itu data yang ada langsung di pathnya, http://website/api/user/1>
+  ReqQuery?: Record<string, unknown>;
+  // Variabel yang di-set secara lokal di variabel res. Untuk pass info ke handler selanjutnya.
   Locals?: Record<string, unknown>;
 };
 
@@ -22,29 +29,14 @@ type RequestHandlerInjector<Options extends Required<RequestHandlerOptions>> = R
   Options["Locals"]
 >;
 
+/**
+ * Wrapper untuk RequestHandler.
+ * Ngubah RequestHandler<A, B, ...> jadi RequestHandler<{A: foo, B: bar, ...}>
+ * Properti yang nggak ditulis otomatis dijadiin Record<string, never>
+ */
 export type RH<Options extends RequestHandlerOptions = Required<RequestHandlerOptions>> =
   RequestHandlerInjector<FillOptionals<Options>>;
 
-/**
- * RequestHandler itu fungsi.
- *
- * Params, ReqBody, ReqQuery, dan Locals itu properti di parameter fungsi.
- * Mereka contravariant terhadap generic kita.
- *
- * ResBody beda sendiri, dia itu parameter di method pada parameter fungsi
- * Dia covariant terhadap generic kita
- */
 export type ExtractRH<T> = T extends RH<infer O> ? FillOptionals<O> : never;
-
-// Untuk template bisa ikutin ini:
-// export const fn: RH<
-//   EmptyParams,
-//   EmptyResBody,
-//   EmptyReqBody,
-//   EmptyReqQuery,
-//   EmptyLocals
-// > = function (req, res) {
-//   return;
-// };
 
 // Lalu register fungsinya ke index.ts, di _api dan juga app
