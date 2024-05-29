501b8661290eb0d082e406594bbdd2e31083d923
vincentiusvin <vincentius_vin@yahoo.com>
2024-05-29 09:24:52 +0700

    Logic done

--- backend/src/helpers/validate.ts
+++ backend/src/helpers/validate.ts
@@ -1,4 +1,5 @@
-import { AuthError } from "./error";
+import { db } from "../db/db";
+import { AuthError, NotFoundError } from "./error";
 import { RH } from "./types";
 
 export const validateLogged: RH<{
@@ -8,5 +9,25 @@ export const validateLogged: RH<{
     next();
   } else {
     throw new AuthError("You need to be logged in!");
+  }
+};
+
+export const validateProjectID: RH<{
+  Params: {
+    project_id: string;
+  };
+}> = async function (req, res, next) {
+  const project_id = Number(req.params.project_id);
+
+  const project = await db
+    .selectFrom("ms_projects")
+    .select("ms_projects.id")
+    .where("id", "=", project_id)
+    .executeTakeFirst();
+
+  if (!project || project.id !== project_id) {
+    throw new NotFoundError("Projek tidak dapat ditemukan!");
+  } else {
+    next();
   }
 };
--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -2,50 +2,55 @@ import { ExpressionBuilder } from "kysely";
 import { jsonArrayFrom } from "kysely/helpers/postgres";
 import { db } from "../db/db";
 import { DB } from "../db/db_types";
-import { ClientError, NotFoundError } from "../helpers/error";
+import { AuthError, ClientError, NotFoundError } from "../helpers/error";
 import { RH } from "../helpers/types";
 
-function withDevs(eb: ExpressionBuilder<DB, "ms_projects">) {
+const ProjectRoles = {
+  PENDING: "Pending",
+  DEV: "Dev",
+  ADMIN: "Admin",
+} as const;
+
+type Roles = (typeof ProjectRoles)[keyof typeof ProjectRoles];
+
+export function withMembers(eb: ExpressionBuilder<DB, "ms_projects">) {
   return jsonArrayFrom(
     eb
       .selectFrom("projects_users")
       .innerJoin("ms_users", "ms_users.id", "projects_users.user_id")
-      .select(["ms_users.id", "ms_users.name"])
+      .select(["ms_users.id", "ms_users.name", "projects_users.role"])
       .whereRef("projects_users.project_id", "=", "ms_projects.id"),
   );
 }
 
-function withOrgMembers(eb: ExpressionBuilder<DB, "ms_projects">) {
-  return jsonArrayFrom(
-    eb
-      .selectFrom("ms_orgs")
-      .innerJoin("orgs_users", "orgs_users.org_id", "ms_orgs.id")
-      .innerJoin("ms_users", "ms_users.id", "orgs_users.user_id")
-      .select(["ms_users.id", "ms_users.name"])
-      .whereRef("ms_orgs.id", "=", "ms_projects.org_id"),
-  );
-}
-
-export async function getProjectMembers(project_id: number) {
-  const project = await db
-    .selectFrom("ms_projects")
-    .select((eb) => [withDevs(eb).as("project_devs"), withOrgMembers(eb).as("org_members")])
-    .where("ms_projects.id", "=", project_id)
+async function getProjectRole(user_id: number, project_id: number): Promise<Roles | undefined> {
+  const res = await db
+    .selectFrom("projects_users")
+    .select("role")
+    .where((eb) =>
+      eb.and({
+        "projects_users.user_id": user_id,
+        "projects_users.project_id": project_id,
+      }),
+    )
     .executeTakeFirst();
 
-  if (project === undefined) {
-    throw new NotFoundError("Projek tidak dapat ditemukan!");
+  if (!res) {
+    return undefined;
   }
 
-  return {
-    project_devs: project.project_devs.map((x) => x.id),
-    org_members: project.org_members.map((x) => x.id),
-  };
+  for (const role of Object.values(ProjectRoles)) {
+    if (res.role === role) {
+      return role;
+    }
+  }
+
+  throw new Error("Role user tidak diketahui!");
 }
 
 export const getProjectsDetailMembersDetail: RH<{
   ResBody: {
-    status: "Member" | "Dev" | "Not Involved";
+    role: "Admin" | "Dev" | "Pending";
   };
   Params: {
     project_id: string;
@@ -56,14 +61,11 @@ export const getProjectsDetailMembersDetail: RH<{
   const project_id = Number(project_id_str);
   const user_id = Number(user_id_str);
 
-  const result = await getProjectMembers(project_id);
-
-  if (result.org_members.includes(user_id)) {
-    res.json({ status: "Member" });
-  } else if (result.project_devs.includes(user_id)) {
-    res.json({ status: "Dev" });
+  const result = await getProjectRole(user_id, project_id);
+  if (!result) {
+    throw new NotFoundError("User tidak terdaftar dalam projek!");
   } else {
-    res.json({ status: "Not Involved" });
+    res.json({ role: result });
   }
 };
 
@@ -71,6 +73,9 @@ export const putProjectsDetailMembersDetail: RH<{
   ResBody: {
     msg: string;
   };
+  ReqBody: {
+    role: Roles;
+  };
   Params: {
     project_id: string;
     user_id: string;
@@ -79,26 +84,22 @@ export const putProjectsDetailMembersDetail: RH<{
   const { project_id: project_id_str, user_id: user_id_str } = req.params;
   const project_id = Number(project_id_str);
   const user_id = Number(user_id_str);
+  const sender_id = req.session.user_id!;
+  const role = req.body.role;
 
-  const involved = await db
-    .selectFrom("ms_projects")
-    .select((eb) => [withDevs(eb).as("project_devs"), withOrgMembers(eb).as("org_members")])
-    .where("ms_projects.id", "=", project_id)
-    .executeTakeFirst();
+  const sender_role = await getProjectRole(sender_id, project_id);
 
-  if (!involved) {
-    throw new NotFoundError("Projek tidak dapat ditemukan!");
+  let allowed = false;
+  // Admin boleh ngelakuin apapun.
+  // Dev cuma boleh apply kalau belum terlibat. Kalau mau leave pakai delete, bukan method ini.
+  if (sender_role === ProjectRoles.ADMIN) {
+    allowed = true;
+  } else if (sender_id === user_id && sender_role === undefined && ProjectRoles.PENDING) {
+    allowed = true;
   }
 
-  const org_members = involved.org_members.map((x) => x.id);
-  const project_devs = involved.project_devs.map((x) => x.id);
-
-  if (org_members.includes(user_id)) {
-    throw new ClientError("Pengguna sudah terlibat melalui organisasi!");
-  }
-
-  if (project_devs.includes(user_id)) {
-    throw new ClientError("Pengguna sudah terlibat dalam projek!");
+  if (!allowed) {
+    throw new AuthError("Anda tidak memiliki akses untuk melakukan aksi ini!");
   }
 
   await db
@@ -106,9 +107,9 @@ export const putProjectsDetailMembersDetail: RH<{
     .values({
       project_id: project_id,
       user_id: user_id,
+      role: role,
     })
     .execute();
-
   res.json({ msg: "Pengguna berhasil ditambahkan!" });
 };
 
@@ -124,26 +125,19 @@ export const deleteProjectsDetailMembersDetail: RH<{
   const { project_id: project_id_str, user_id: user_id_str } = req.params;
   const project_id = Number(project_id_str);
   const user_id = Number(user_id_str);
+  const sender_id = req.session.user_id!;
 
-  const involved = await db
-    .selectFrom("ms_projects")
-    .select((eb) => [withDevs(eb).as("project_devs"), withOrgMembers(eb).as("org_members")])
-    .where("ms_projects.id", "=", project_id)
-    .executeTakeFirst();
+  const sender_role = await getProjectRole(sender_id, project_id);
 
-  if (!involved) {
-    throw new NotFoundError("Projek tidak dapat ditemukan!");
+  let allowed = false;
+  if (sender_role === ProjectRoles.ADMIN) {
+    allowed = true;
+  } else if (sender_id === user_id) {
+    allowed = true;
   }
 
-  const org_members = involved.org_members.map((x) => x.id);
-  const project_devs = involved.project_devs.map((x) => x.id);
-
-  if (org_members.includes(user_id)) {
-    throw new ClientError("Pengguna terlibat melalui organisasi dan tidak dapat dihapus!");
-  }
-
-  if (!project_devs.includes(user_id)) {
-    throw new ClientError("Pengguna tidak terlibat dalam projek!");
+  if (!allowed) {
+    throw new AuthError("Anda tidak memiliki akses untuk melakukan aksi ini!");
   }
 
   await db
@@ -188,9 +182,10 @@ export const getProjectsDetail: RH<{
     project_name: string;
     org_id: number;
     project_desc: string;
-    project_devs: {
+    project_members: {
       id: number;
       name: string;
+      role: string; // todo
     }[];
     project_categories: string[];
   };
@@ -206,13 +201,7 @@ export const getProjectsDetail: RH<{
       "ms_projects.name as project_name",
       "ms_projects.org_id",
       "ms_projects.description as project_desc",
-      jsonArrayFrom(
-        eb
-          .selectFrom("ms_users")
-          .innerJoin("projects_users", "ms_users.id", "projects_users.user_id")
-          .select(["ms_users.id", "ms_users.name"])
-          .whereRef("projects_users.project_id", "=", "ms_projects.id"),
-      ).as("project_devs"),
+      withMembers(eb).as("project_members"),
       jsonArrayFrom(
         eb
           .selectFrom("categories_projects")
