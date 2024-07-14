d6e1807a58e40d6f23f0bb81d8656954e2198312
vincentiusvin <vincentius_vin@yahoo.com>
2024-07-14 20:15:51 +0700

    Comment out unneeded code

--- frontend/src/queries/task_hooks.ts
+++ frontend/src/queries/task_hooks.ts
@@ -4,26 +4,17 @@ import { APIContext } from "../helpers/fetch.ts";
 import { queryClient } from "../helpers/queryclient.tsx";
 import { useProjectsDetailBucketsGet } from "./project_hooks.ts";
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export function useFormattedTasks(opts: { project_id: number }) {
   const { project_id } = opts;
   const { data: buckets } = useProjectsDetailBucketsGet({ project_id });
 
   return useQueries({
     queries:
-      buckets?.map((bucket, i) => {
+      buckets?.map((bucket) => {
         return {
           queryKey: ["projects", "detail", "buckets", bucket.id],
           queryFn: () =>
-            new APIContext("BucketsDetailTasksGet")
-              .fetch(`/api/buckets/${bucket.id}/tasks`)
-              .then(async (x) => {
-                await sleep(i * 2000);
-                return x;
-              }),
+            new APIContext("BucketsDetailTasksGet").fetch(`/api/buckets/${bucket.id}/tasks`),
         };
       }) || [],
     combine: (res) => {
