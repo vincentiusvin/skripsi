e383b133fcae2f7094e6158d0e510275a41b926c
vincentiusvin <vincentius_vin@yahoo.com>
2024-07-28 17:07:32 +0700

    Fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,24 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import path from "node:path";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  envDir: "../.env",
-  server: {
-    proxy: {
-      "/api": {
-        target: `http://${process.env.PROXY_HOST}:${process.env.PROXY_PORT}`,
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const parent = path.resolve(process.cwd(), "..");
+  const env = loadEnv(mode, parent);
+
+  return {
+    plugins: [react()],
+    envDir: "../.env",
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://${env.VITE_PROXY_HOST}:${env.VITE_PROXY_PORT}`,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
+  };
 });
