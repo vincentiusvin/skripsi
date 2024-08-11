87c6b6ea89d55a51f79c91f77fbc5d3ce08df4f3
vincentiusvin <vincentius_vin@yahoo.com>
2024-08-11 16:37:24 +0700

    frontend hooks

--- frontend/src/queries/org_hooks.ts
+++ frontend/src/queries/org_hooks.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { API } from "../../../backend/src/routes.ts";
 import { APIContext } from "../helpers/fetch";
 import { queryClient } from "../helpers/queryclient";
 
@@ -93,6 +94,63 @@ export function useOrgsDelete(opts: { id: number; onSuccess?: () => void }) {
       queryClient.invalidateQueries({ queryKey: ["orgs"] });
       if (onSuccess) {
         onSuccess();
+      }
+    },
+  });
+}
+
+export function useOrgsDetailMembersGet(opts: { org_id: number; user_id: number }) {
+  const { org_id: project_id, user_id } = opts;
+  return useQuery({
+    queryKey: ["orgs", "detail", project_id, "members", user_id],
+    queryFn: () =>
+      new APIContext("OrgsDetailMembersDetailGet").fetch(
+        `/api/orgs/${project_id}/users/${user_id}`,
+      ),
+  });
+}
+
+export function useOrgsDetailMembersDelete(opts: {
+  org_id: number;
+  user_id: number;
+  onSuccess?: (data: API["OrgsDetailMembersDetailDelete"]["ResBody"]) => void;
+}) {
+  const { org_id, user_id, onSuccess } = opts;
+  return useMutation({
+    mutationFn: () => {
+      return new APIContext("OrgsDetailMembersDetailDelete").fetch(
+        `/api/orgs/${org_id}/users/${user_id}`,
+        {
+          method: "DELETE",
+        },
+      );
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["orgs"] });
+      if (onSuccess) {
+        onSuccess(data);
+      }
+    },
+  });
+}
+
+export function useOrgsDetailMembersPut(opts: {
+  org_id: number;
+  user_id: number;
+  onSuccess?: (data: API["OrgsDetailMembersDetailPut"]["ResBody"]) => void;
+}) {
+  const { org_id, user_id, onSuccess } = opts;
+  return useMutation({
+    mutationFn: new APIContext("OrgsDetailMembersDetailPut").bodyFetch(
+      `/api/orgs/${org_id}/users/${user_id}`,
+      {
+        method: "PUT",
+      },
+    ),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["orgs"] });
+      if (onSuccess) {
+        onSuccess(data);
       }
     },
   });
