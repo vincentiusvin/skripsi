7bb735989304f86904be85682d565c278168b910
vincentiusvin <vincentius_vin@yahoo.com>
2024-08-31 21:54:09 +0700

    dont look

--- backend/src/modules/user/user.test.ts
+++ backend/src/modules/user/user.test.ts
@@ -4,7 +4,7 @@ import { Application } from "../../app.js";
 import { APIContext, baseCase, getLoginCookie } from "../../test/helpers.js";
 import { clearDB } from "../../test/setup-test.js";
 
-describe.only("users api", () => {
+describe("users api", () => {
   let app: Application;
   let caseData: Awaited<ReturnType<typeof baseCase>>;
 
