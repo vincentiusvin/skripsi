a36cee441f81a7528f381db44e71946fdcc49a7b
vincentiusvin <vincentius_vin@yahoo.com>
2024-09-21 17:48:13 +0700

    Notification more

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -13,6 +13,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import {
   AppBar,
   Avatar,
+  Badge,
   Box,
   Button,
   Dialog,
@@ -32,7 +33,7 @@ import {
 import dayjs from "dayjs";
 import { useState } from "react";
 import { useAppTheme } from "../helpers/theme.ts";
-import { useNotificationsGet } from "../queries/notification_hooks.ts";
+import { useNotificationsGet, useNotificationsPut } from "../queries/notification_hooks.ts";
 import { useSessionDelete, useSessionGet } from "../queries/sesssion_hooks";
 import { useUsersDetailGet } from "../queries/user_hooks";
 import StyledLink from "./StyledLink.tsx";
@@ -53,17 +54,77 @@ function UserImage(props: { user_id: number }) {
   );
 }
 
+function NotificationEntry(props: {
+  notification_data: NonNullable<ReturnType<typeof useNotificationsGet>["data"]>[number];
+}) {
+  const { notification_data } = props;
+  const { mutate } = useNotificationsPut({
+    notification_id: notification_data.id,
+  });
+
+  return (
+    <Paper
+      key={notification_data.id}
+      sx={{
+        paddingX: 4,
+        paddingY: 2,
+      }}
+    >
+      <Typography
+        variant="h6"
+        fontWeight={"bold"}
+        sx={{
+          wordBreak: "break-word",
+        }}
+      >
+        {notification_data.title}
+      </Typography>
+      <Typography
+        sx={{
+          wordBreak: "break-word",
+        }}
+      >
+        {notification_data.description}
+      </Typography>
+      <Stack direction={"row"} alignItems={"center"}>
+        <Typography variant="caption">
+          {dayjs(notification_data.created_at).format("ddd[,] D[/]M[/]YY HH:mm")}
+        </Typography>
+        <Box flexGrow={1}></Box>
+        {notification_data.read ? (
+          <Button onClick={() => mutate({ read: false })}>
+            <Typography variant="caption">Tandai sebagai belum dibaca</Typography>
+          </Button>
+        ) : (
+          <Button onClick={() => mutate({ read: true })}>
+            <Typography variant="caption">Tandai sebagai dibaca</Typography>
+          </Button>
+        )}
+      </Stack>
+    </Paper>
+  );
+}
+
 function NotificationDialog(props: { user_id: number }) {
   const { user_id } = props;
   const { data: notification } = useNotificationsGet({
     user_id,
   });
   const [openNotification, setOpenNotification] = useState(false);
+  const unread = notification?.filter((x) => x.read == false).length;
   return (
     <>
-      <IconButton onClick={() => setOpenNotification(true)}>
-        <Notifications />
-      </IconButton>
+      {unread != undefined ? (
+        <Badge badgeContent={unread} color="primary">
+          <IconButton onClick={() => setOpenNotification(true)}>
+            <Notifications />
+          </IconButton>
+        </Badge>
+      ) : (
+        <IconButton onClick={() => setOpenNotification(true)}>
+          <Notifications />
+        </IconButton>
+      )}
       <Dialog open={openNotification} onClose={() => setOpenNotification(false)}>
         <DialogTitle>Notifikasi</DialogTitle>
         <DialogContent
@@ -77,33 +138,7 @@ function NotificationDialog(props: { user_id: number }) {
           {notification != undefined ? (
             <Stack direction={"column"} spacing={2}>
               {notification.map((x) => (
-                <Paper
-                  key={x.id}
-                  sx={{
-                    paddingX: 4,
-                    paddingY: 2,
-                  }}
-                >
-                  <Typography
-                    variant="h6"
-                    fontWeight={"bold"}
-                    sx={{
-                      wordBreak: "break-word",
-                    }}
-                  >
-                    {x.title}
-                  </Typography>
-                  <Typography
-                    sx={{
-                      wordBreak: "break-word",
-                    }}
-                  >
-                    {x.description}
-                  </Typography>
-                  <Typography variant="caption">
-                    {dayjs(x.created_at).format("ddd[,] D[/]M[/]YY HH:mm")}
-                  </Typography>
-                </Paper>
+                <NotificationEntry key={x.id} notification_data={x} />
               ))}
             </Stack>
           ) : (
