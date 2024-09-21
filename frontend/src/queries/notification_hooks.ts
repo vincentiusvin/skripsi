8c6d81a389ebe0ea4c440bc2d1c0728f3a781c35
vincentiusvin <vincentius_vin@yahoo.com>
2024-09-21 16:05:37 +0700

    hooks

---
+++ frontend/src/queries/notification_hooks.ts
@@ -0,0 +1,54 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { APIContext } from "../helpers/fetch.ts";
+import { queryClient } from "../helpers/queryclient.tsx";
+
+const notifKeys = {
+  all: () => ["notifications"] as const,
+  lists: () => [...notifKeys.all(), "list"] as const,
+  list: (opts?: { user_id?: number; read?: boolean }) =>
+    [...notifKeys.all(), "list", opts] as const,
+  details: () => [...notifKeys.all(), "detail"] as const,
+  detail: (notif_id: number) => [...notifKeys.details(), notif_id] as const,
+};
+
+export function useNotificationsGet(opts: {
+  user_id?: number;
+  read?: boolean;
+  retry?: (failureCount: number, error: Error) => boolean;
+}) {
+  const { user_id, read, retry } = opts;
+  let read_string: "true" | "false" | undefined = undefined;
+  if (read != undefined) {
+    read_string = read ? "true" : "false";
+  }
+
+  return useQuery({
+    queryKey: notifKeys.list({ user_id }),
+    queryFn: () =>
+      new APIContext("NotificationsGet").fetch(`/api/notifications`, {
+        query: {
+          user_id: user_id?.toString(),
+          read: read_string,
+        },
+      }),
+    retry: retry,
+  });
+}
+
+export function useNotificationsPut(opts: { notification_id: number; onSuccess?: () => void }) {
+  const { notification_id, onSuccess } = opts;
+  return useMutation({
+    mutationFn: new APIContext("NotificationsPut").bodyFetch(
+      `/api/notifications/${notification_id}`,
+      {
+        method: "PUT",
+      },
+    ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: notifKeys.all() });
+      if (onSuccess) {
+        onSuccess();
+      }
+    },
+  });
+}
