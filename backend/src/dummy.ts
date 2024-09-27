ec1933d8d9f7d0b36ae5f6025a37b191c48c8cbf
vincentiusvin <vincentius_vin@yahoo.com>
2024-09-27 11:10:07 +0700

    Keep migration data

--- backend/src/dummy.ts
+++ backend/src/dummy.ts
@@ -1,12 +1,8 @@
 import { Application } from "./app.js";
 import { baseCase } from "./test/helpers.js";
-import { clearDB } from "./test/setup-test.js";
 
 async function dummy() {
   const app = Application.getApplication();
-  console.log("Clearing db...");
-  await clearDB(app);
-  console.log("DB cleared!");
   console.log("Inserting dummy data...");
   await baseCase(app);
   console.log("Dummy data inserted!");
