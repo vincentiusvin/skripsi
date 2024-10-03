45fc133a4e945723ba35871e188255c919aac6f0
vincentiusvin <vincentius_vin@yahoo.com>
2024-10-03 18:28:09 +0700

    ok?

--- backend/src/modules/user/UserController.ts
+++ backend/src/modules/user/UserController.ts
@@ -1,7 +1,6 @@
 import type { Express } from "express";
 import { z } from "zod";
 import { Controller, Route } from "../../helpers/controller.js";
-import { RH } from "../../helpers/types.js";
 import { UserService } from "./UserService.js";
 
 export class UserController extends Controller {
@@ -13,167 +12,110 @@ export class UserController extends Controller {
 
   init() {
     return {
-      UsersPost: new Route({
-        handler: this.postUser,
-        method: "post",
-        path: "/api/users",
-        schema: {
-          ReqBody: z.object({
-            user_name: z.string().min(1, "Username tidak boleh kosong!"),
-            user_password: z.string().min(1, "Password tidak boleh kosong!"),
-          }),
-        },
-      }),
-
-      UsersGet: new Route({
-        handler: this.getUsers,
-        method: "get",
-        path: "/api/users",
-      }),
-
-      UsersDetailGet: new Route({
-        handler: this.getUserDetail,
-        method: "get",
-        path: "/api/users/:id",
-        schema: {
-          Params: z.object({
-            id: z
-              .string()
-              .min(1)
-              .refine((arg) => !isNaN(Number(arg)), { message: "ID pengguna tidak valid" }),
-          }),
-        },
-      }),
-
-      UsersDetailPut: new Route({
-        handler: this.updateUserAccount,
-        method: "put",
-        path: "/api/users/:id",
-        schema: {
-          Params: z.object({
-            id: z
-              .string()
-              .min(1)
-              .refine((arg) => !isNaN(Number(arg)), { message: "ID pengguna tidak valid!" }),
-          }),
-          ReqBody: z.object({
-            user_name: z.string().min(1, "Nama tidak boleh kosong!").optional(),
-            user_password: z.string().min(1, "Password tidak boleh kosong!").optional(),
-            user_education_level: z.string().min(1, "Pendidikan tidak boleh kosong!").optional(),
-            user_school: z.string().min(1, "Sekolah tidak boleh kosong!").optional(),
-            user_about_me: z.string().min(1, "Biodata tidak boleh kosong!").optional(),
-            user_image: z.string().min(1, "Gambar tidak boleh kosong!").optional(),
-          }),
-        },
-      }),
+      UsersPost: this.UsersPost,
+      UsersGet: this.UsersGet,
+      UsersDetailGet: this.UsersDetailGet,
+      UsersDetailPut: this.UsersDetailPut,
     };
   }
+  UsersPost = new Route({
+    handler: async (req, res) => {
+      const { user_name, user_password } = req.body;
 
-  private postUser: RH<{
-    ResBody: { user_id: number; user_name: string };
-    ReqBody: { user_name: string; user_password: string };
-  }> = async (req, res) => {
-    const { user_name, user_password } = req.body;
+      const user_id = await this.user_service.addUser(user_name, user_password);
+      if (user_id == undefined) {
+        throw new Error("Gagal menambahkan pengguna!");
+      }
 
-    const user_id = await this.user_service.addUser(user_name, user_password);
-    if (user_id == undefined) {
-      throw new Error("Gagal menambahkan pengguna!");
-    }
+      const result = await this.user_service.getUserDetail(user_id.id);
+      if (result == undefined) {
+        throw new Error("Gagal menambahkan pengguna!");
+      }
 
-    const result = await this.user_service.getUserDetail(user_id.id);
-    if (result == undefined) {
-      throw new Error("Gagal menambahkan pengguna!");
-    }
-
-    res.status(201).json(result);
-  };
-
-  private getUsers: RH<{
-    ResBody: {
-      user_id: number;
-      user_name: string;
-      user_is_admin: boolean;
-      user_email: string | null;
-      user_education_level: string | null;
-      user_school: string | null;
-      user_about_me: string | null;
-      user_image: string | null;
-      user_created_at: Date;
-    }[];
-  }> = async (req, res) => {
-    const result = await this.user_service.getUsers();
-    res.status(200).json(result);
-  };
-
-  private getUserDetail: RH<{
-    Params: { id: string };
-    ResBody: {
-      user_id: number;
-      user_is_admin: boolean;
-      user_name: string;
-      user_email: string | null;
-      user_education_level: string | null;
-      user_school: string | null;
-      user_about_me: string | null;
-      user_image: string | null;
-      user_created_at: Date;
-    };
-  }> = async (req, res) => {
-    const id = Number(req.params.id);
-    const result = await this.user_service.getUserDetail(id);
-    res.status(200).json(result);
-  };
-
-  private updateUserAccount: RH<{
-    ResBody: {
-      user_id: number;
-      user_name: string;
-      user_email: string | null;
-      user_education_level: string | null;
-      user_school: string | null;
-      user_about_me: string | null;
-      user_created_at: Date;
-      user_image: string | null;
-    };
-    Params: {
-      id: string;
-    };
-    ReqBody: {
-      user_name?: string;
-      user_password?: string;
-      user_email?: string;
-      user_education_level?: string;
-      user_school?: string;
-      user_about_me?: string;
-      user_image?: string;
-    };
-  }> = async (req, res) => {
-    const {
-      user_name,
-      user_password,
-      user_email,
-      user_education_level,
-      user_school,
-      user_about_me,
-      user_image,
-    } = req.body;
-    const user_id = Number(req.params.id);
-    const sender_id = Number(req.session.user_id);
-
-    await this.user_service.updateAccountDetail(
-      user_id,
-      {
+      res.status(201).json(result);
+    },
+    method: "post",
+    path: "/api/users",
+    schema: {
+      ReqBody: z.object({
+        user_name: z.string().min(1, "Username tidak boleh kosong!"),
+        user_password: z.string().min(1, "Password tidak boleh kosong!"),
+      }),
+    },
+  });
+  UsersGet = new Route({
+    handler: async (req, res) => {
+      const result = await this.user_service.getUsers();
+      res.status(200).json(result);
+    },
+    method: "get",
+    path: "/api/users",
+  });
+  UsersDetailGet = new Route({
+    handler: async (req, res) => {
+      const id = Number(req.params.id);
+      const result = await this.user_service.getUserDetail(id);
+      res.status(200).json(result);
+    },
+    method: "get",
+    path: "/api/users/:id",
+    schema: {
+      Params: z.object({
+        id: z
+          .string()
+          .min(1)
+          .refine((arg) => !isNaN(Number(arg)), { message: "ID pengguna tidak valid" }),
+      }),
+    },
+  });
+  UsersDetailPut = new Route({
+    method: "put",
+    path: "/api/users/:id",
+    schema: {
+      Params: z.object({
+        id: z
+          .string()
+          .min(1)
+          .refine((arg) => !isNaN(Number(arg)), { message: "ID pengguna tidak valid!" }),
+      }),
+      ReqBody: z.object({
+        user_name: z.string().min(1, "Nama tidak boleh kosong!").optional(),
+        user_password: z.string().min(1, "Password tidak boleh kosong!").optional(),
+        user_education_level: z.string().min(1, "Pendidikan tidak boleh kosong!").optional(),
+        user_school: z.string().min(1, "Sekolah tidak boleh kosong!").optional(),
+        user_about_me: z.string().min(1, "Biodata tidak boleh kosong!").optional(),
+        user_image: z.string().min(1, "Gambar tidak boleh kosong!").optional(),
+        user_email: z.string().min(1, "Email tidak boleh kosong!").optional(),
+      }),
+    },
+    handler: async (req, res) => {
+      const {
         user_name,
+        user_password,
         user_email,
         user_education_level,
         user_school,
         user_about_me,
         user_image,
-        user_password,
-      },
-      sender_id,
-    );
-    const updated = await this.user_service.getUserDetail(user_id);
-    res.status(200).json(updated);
-  };
+      } = req.body;
+      const user_id = Number(req.params.id);
+      const sender_id = Number(req.session.user_id);
+
+      await this.user_service.updateAccountDetail(
+        user_id,
+        {
+          user_name,
+          user_email,
+          user_education_level,
+          user_school,
+          user_about_me,
+          user_image,
+          user_password,
+        },
+        sender_id,
+      );
+      const updated = await this.user_service.getUserDetail(user_id);
+      res.status(200).json(updated);
+    },
+  });
 }
