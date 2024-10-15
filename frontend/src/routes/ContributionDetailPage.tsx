90ab1482e2430c9dd95074a822c5cdd5e12496ff
vincentiusvin <vincentius_vin@yahoo.com>
2024-10-15 16:37:20 +0700

    kudu bikin backend

--- frontend/src/routes/ContributionDetailPage.tsx
+++ frontend/src/routes/ContributionDetailPage.tsx
@@ -1,3 +1,12 @@
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+  timelineItemClasses,
+} from "@mui/lab";
 import { Divider, Skeleton, Stack, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import dayjs from "dayjs";
@@ -5,6 +14,60 @@ import { useParams } from "wouter";
 import ProjectCard from "../components/Cards/ProjectCard.tsx";
 import UserLabel from "../components/UserLabel.tsx";
 import { useContributionsDetailGet } from "../queries/contribution_hooks.ts";
+
+function ContributionApproval() {
+  const steps = [
+    {
+      title: "Dikumpul oleh developer.",
+      desc: "Bukti kontribusi dibuat dan diisi oleh developer.",
+      stat: "pass",
+    },
+    {
+      title: "Menunggu persetujuan organisasi",
+      desc: "Bukti kontribusi perlu disetujui terlebih dahulu oleh pengurus organisasi",
+      stat: "here",
+    },
+    {
+      title: "Disetujui",
+      desc: "Bukti kontribusi sudah disetujui oleh pengurus organisasi dan dapat dilihat secara publik.",
+      stat: "no",
+    },
+  ];
+  return (
+    <>
+      <Divider />
+      <Typography variant="h6" fontWeight={"bold"}>
+        Persetujuan
+      </Typography>
+      <Timeline
+        sx={{
+          [`*.${timelineItemClasses.root}:before`]: {
+            flex: 0,
+            padding: 0,
+          },
+        }}
+      >
+        {steps.map((x, i) => (
+          <TimelineItem key={i}>
+            <TimelineSeparator>
+              <TimelineDot
+                color={x.stat === "pass" ? "success" : x.stat === "here" ? "warning" : undefined}
+              />
+              {i !== steps.length - 1 ? <TimelineConnector /> : null}
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography variant="body1">
+                {x.title}
+                {x.stat === "here" ? " (anda di sini)" : null}
+              </Typography>
+              <Typography variant="caption">{x.desc}</Typography>
+            </TimelineContent>
+          </TimelineItem>
+        ))}
+      </Timeline>
+    </>
+  );
+}
 
 function ContributionDetail(props: { contribution_id: number }) {
   const { contribution_id } = props;
@@ -55,6 +118,7 @@ function ContributionDetail(props: { contribution_id: number }) {
             Proyek
           </Typography>
           <ProjectCard project_id={contrib.project_id} />
+          <ContributionApproval />
         </Stack>
       </Grid>
     </Grid>
