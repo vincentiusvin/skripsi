3e34afaa4513634325f3a3aedfdd0f1a20c36944
vincentiusvin <vincentius_vin@yahoo.com>
2024-10-24 07:38:34 +0700

    docs

--- backend/src/helpers/transaction/transaction.ts
+++ backend/src/helpers/transaction/transaction.ts
@@ -1,6 +1,9 @@
 import { Kysely } from "kysely";
 import { DB } from "../../db/db_types.js";
 
+/**
+ * Manager buat transaksi DB.
+ */
 export class TransactionManager {
   private db: Kysely<DB>;
   constructor(db: Kysely<DB>) {
@@ -11,6 +14,16 @@ export class TransactionManager {
     return this.db;
   }
 
+  /**
+   * Mulai transaksi baru ATAU join transaksi yang udah jalan.
+   * Kalau udah ada transaksi, parameter di callbacknya itu obj.
+   * Kalau belum ada, bakal dibikin objek baru.
+   *
+   * @param obj - Service yang mau ditransaksiin. Harus implements Transactable.
+   * @param cb - Callback yang mau dijalanin.
+   * Pastiin yang dipakai cuma objek yang ada di parameter callback.
+   * Jangan kebablasan pakai `this`
+   */
   async transaction<T extends Transactable<T>, R>(obj: T, cb: (x: T) => Promise<R>) {
     if (this.db.isTransaction) {
       return await cb(obj); // serv === this
