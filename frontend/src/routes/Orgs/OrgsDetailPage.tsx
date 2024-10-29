8d51cfb2cf0819dcdb99590831a0202d3cd2ce67
vincentiusvin <vincentius_vin@yahoo.com>
2024-10-29 16:35:25 +0700

    splits

--- frontend/src/routes/Orgs/OrgsDetailPage.tsx
+++ frontend/src/routes/Orgs/OrgsDetailPage.tsx
@@ -1,4 +1,7 @@
+import { Home, Phone, Visibility } from "@mui/icons-material";
 import {
+  Avatar,
+  Box,
   Button,
   Chip,
   Dialog,
@@ -6,6 +9,7 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Divider,
   Paper,
   Skeleton,
   Stack,
@@ -15,6 +19,7 @@ import Grid from "@mui/material/Grid2";
 import { useState } from "react";
 import { useParams } from "wouter";
 import ProjectCard from "../../components/Cards/ProjectCard.tsx";
+import { useNavigation } from "../../components/Navigation/NavigationContext.ts";
 import {
   useOrgDetailGet,
   useOrgsDetailMembersDelete,
@@ -71,13 +76,92 @@ function InvitedDialog(props: { org_id: number; user_id: number }) {
   );
 }
 
-function OrgsInfo(props: { org_id: number }) {
+function OrgsBasicData(props: { org_id: number }) {
   const { org_id } = props;
   const { data } = useOrgDetailGet({
     id: org_id,
   });
-  const { data: projectData } = useProjectsGet({
-    org_id: org_id,
+  const [nav, setNav] = useNavigation();
+
+  if (!data) {
+    return <Skeleton />;
+  }
+
+  return (
+    <Grid container alignItems={"center"}>
+      <Grid
+        size={{
+          xs: 12,
+          md: 2,
+        }}
+      >
+        <Avatar
+          sx={{
+            height: 128,
+            width: 128,
+          }}
+          variant="rounded"
+        >
+          {data.org_image}
+        </Avatar>
+      </Grid>
+      <Grid
+        size={{
+          xs: 12,
+          md: 10,
+        }}
+      >
+        <Stack spacing={2}>
+          <Typography variant="h4" fontWeight={"bold"}>
+            {data.org_name}
+          </Typography>
+          <Stack spacing={1} direction={"row"} gap={2} flexWrap={"wrap"}>
+            {data.org_categories.map((category) => (
+              <Chip label={category.category_name} key={category.category_id} />
+            ))}
+          </Stack>
+          <Stack direction={"row"} spacing={4}>
+            <Stack direction="row" alignItems={"center"} spacing={2}>
+              <Home />
+              <Box>
+                <Typography fontWeight="bold">Alamat</Typography>
+                <Typography>{data.org_address}</Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" alignItems={"center"} spacing={2}>
+              <Phone />
+              <Box>
+                <Typography fontWeight="bold">Telepon</Typography>
+                <Typography>{data.org_phone}</Typography>
+              </Box>
+            </Stack>
+          </Stack>
+          <Box>
+            <Button
+              variant="contained"
+              startIcon={<Visibility />}
+              onClick={() => {
+                setNav({
+                  type: "orgs",
+                  id: org_id,
+                  open: true,
+                });
+              }}
+              disabled={nav.type === "orgs" && nav.id === org_id && nav.open}
+            >
+              Buka di Dashboard
+            </Button>
+          </Box>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+}
+
+function OrgsPeople(props: { org_id: number }) {
+  const { org_id } = props;
+  const { data } = useOrgDetailGet({
+    id: org_id,
   });
 
   if (!data) {
@@ -85,61 +169,41 @@ function OrgsInfo(props: { org_id: number }) {
   }
 
   return (
-    <Stack spacing={4}>
-      <Typography variant="h4" fontWeight={"bold"} align="center">
-        {data.org_name}
+    <Stack spacing={2}>
+      <Typography variant="h6" fontWeight={"bold"}>
+        Pengurus
       </Typography>
-      <Paper
-        sx={{
-          p: 2,
-        }}
-      >
-        <Typography variant="h4" fontWeight="bold" textAlign={"center"}>
-          Tentang Kami
-        </Typography>
-        <Typography textAlign={"center"}>{data.org_description}</Typography>
-        <Typography variant="h4" fontWeight="bold" textAlign={"center"}>
-          Alamat
-        </Typography>
-        <Typography textAlign={"center"}>{data.org_address}</Typography>
-        <Typography variant="h4" fontWeight="bold" textAlign={"center"}>
-          Hubungi Kami
-        </Typography>
-        <Typography textAlign={"center"}>{data.org_phone}</Typography>
-        <Typography variant="h4" fontWeight="bold" textAlign={"center"}>
-          Anggota
-        </Typography>
-        <Grid container width={"85%"} margin={"0 auto"} spacing={2} columnSpacing={4}>
-          {data.org_users
-            .filter((x) => x.user_role === "Admin")
-            .map((x) => (
-              <Grid
-                justifyContent={"center"}
-                key={x.user_id}
-                size={{
-                  xs: 12,
-                  md: 6,
-                  lg: 4,
-                }}
-              >
-                <OrgMember user_id={x.user_id} org_id={org_id} />
-              </Grid>
-            ))}
-        </Grid>
-        <Typography textAlign={"center"} variant="h4" fontWeight={"bold"}>
-          Kategori
-        </Typography>
-        <Stack spacing={1} direction={"row"} justifyContent={"center"}>
-          {data.org_categories.map((category) => (
-            <Chip label={category.category_name} key={category.category_id} />
+      <Divider />
+      <Stack spacing={1}>
+        {data.org_users
+          .filter((x) => x.user_role === "Admin")
+          .map((x) => (
+            <OrgMember key={x.user_id} user_id={x.user_id} org_id={org_id} />
           ))}
-        </Stack>
-      </Paper>
+      </Stack>
+    </Stack>
+  );
+}
+
+function OrgsProjects(props: { org_id: number }) {
+  const { org_id } = props;
+
+  const { data: projects } = useProjectsGet({
+    org_id: org_id,
+  });
+
+  if (!projects) {
+    return <Skeleton />;
+  }
+
+  return (
+    <Stack spacing={2}>
       <Typography variant="h6" fontWeight={"bold"}>
         Proyek
       </Typography>
+      <Divider />
       <Grid container spacing={2}>
-        {projectData?.map((x, i) => (
+        {projects.map((x, i) => (
           <Grid
             key={i}
             size={{
@@ -152,6 +216,39 @@ function OrgsInfo(props: { org_id: number }) {
           </Grid>
         ))}
       </Grid>
+    </Stack>
+  );
+}
+
+function OrgsInfo(props: { org_id: number }) {
+  const { org_id } = props;
+  const { data } = useOrgDetailGet({
+    id: org_id,
+  });
+
+  if (!data) {
+    return <Skeleton />;
+  }
+
+  return (
+    <Stack spacing={4}>
+      <Paper
+        sx={{
+          py: 2,
+          px: 8,
+        }}
+      >
+        <OrgsBasicData org_id={org_id} />
+      </Paper>
+      <Grid container spacing={4}>
+        <Grid size={10}>
+          <OrgsProjects org_id={org_id} />
+        </Grid>
+        <Grid size={2}>
+          <OrgsPeople org_id={org_id} />
+        </Grid>
+      </Grid>
+      <Typography textAlign={"center"}>{data.org_description}</Typography>
     </Stack>
   );
 }
