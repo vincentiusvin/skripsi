496e5b77a07e563077a9adb3d8d4598e696d8bf1
vincentiusvin <vincentius_vin@yahoo.com>
2024-10-30 17:07:12 +0700

    frontend

--- frontend/src/routes/RegisterPage.tsx
+++ frontend/src/routes/RegisterPage.tsx
@@ -1,7 +1,9 @@
 import {
+  Avatar,
   Box,
   Button,
   Paper,
+  Slide,
   Stack,
   Step,
   StepLabel,
@@ -9,39 +11,20 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import { enqueueSnackbar } from "notistack";
 import { useState } from "react";
 import { useLocation } from "wouter";
+import charityImg from "../assets/help.png";
+import StyledLink from "../components/StyledLink.tsx";
 import { useUsersPost } from "../queries/user_hooks";
 
-function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [step, setStep] = useState(0);
-
-  const [, setLocation] = useLocation();
-
-  const { mutate: postUsers } = useUsersPost({
-    onSuccess: () => {
-      enqueueSnackbar({
-        message: <Typography>Berhasil mendaftarkan akun!</Typography>,
-        autoHideDuration: 5000,
-        variant: "success",
-      });
-      setLocation("/");
-    },
-  });
-
-  function register() {
-    postUsers({
-      user_name: username,
-      user_password: password,
-    });
-  }
+function RegisterSteps(props: { step: number }) {
+  const { step } = props;
 
   return (
-    <Stack spacing={8} mt={2}>
-      <Stepper activeStep={step}>
+    <Slide in={true} direction="up">
+      <Stepper activeStep={step} orientation="vertical">
         <Step>
           <StepLabel>
             <Typography>Informasi akun</Typography>
@@ -64,49 +47,192 @@ function Register() {
           </StepLabel>
         </Step>
       </Stepper>
+    </Slide>
+  );
+}
 
-      <Box
-        sx={{
-          paddingX: 52,
+function ImageSidebar(props: { isStepped: boolean }) {
+  const { isStepped } = props;
+  return (
+    <Slide in={true} direction="down" appear={isStepped}>
+      <Box>
+        <Typography variant="h4" fontWeight={"bold"} textAlign={"center"}>
+          Dev4You
+        </Typography>
+        <Avatar
+          sx={{
+            display: {
+              xs: "none",
+              md: "block",
+            },
+            margin: "auto",
+            width: "75%",
+            height: "100%",
+          }}
+          variant="square"
+          src={charityImg}
+        ></Avatar>
+      </Box>
+    </Slide>
+  );
+}
+
+function SecondStep(props: { cont: () => void; back: () => void }) {
+  const { back, cont } = props;
+  return (
+    <Stack spacing={2}>
+      <Typography variant="h5" fontWeight={"bold"} textAlign={"center"}>
+        Lengkapi Data Diri
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Button fullWidth onClick={() => back()} variant="outlined">
+          Mundur
+        </Button>
+        <Button fullWidth onClick={() => cont()} variant="contained">
+          Lanjut
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
+
+function ThirdStep(props: { cont: () => void; back: () => void }) {
+  const { back, cont } = props;
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button fullWidth onClick={() => back()} variant="outlined">
+        Mundur
+      </Button>
+      <Button fullWidth onClick={() => cont()} variant="contained">
+        Lanjut
+      </Button>
+    </Stack>
+  );
+}
+
+function FirstStep(props: {
+  setUsername: (x: string) => void;
+  setPassword: (x: string) => void;
+  password: string;
+  username: string;
+  cont: () => void;
+}) {
+  const { username, password, setUsername, setPassword, cont } = props;
+  return (
+    <Stack spacing={4}>
+      <Typography variant="h5" fontWeight={"bold"} textAlign={"center"}>
+        Daftar
+      </Typography>
+      <TextField
+        fullWidth
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        label="Username"
+      ></TextField>
+      <TextField
+        fullWidth
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        sx={{ display: "block" }}
+      ></TextField>
+      <Button
+        variant="contained"
+        size="large"
+        fullWidth
+        onClick={() => {
+          cont();
         }}
       >
-        {step === 0 ? (
-          <Paper
-            sx={{
-              paddingX: 4,
-              paddingY: 8,
-            }}
-          >
-            <Stack spacing={4}>
-              <Typography variant="h5" fontWeight={"bold"} textAlign={"center"}>
-                Daftar
-              </Typography>
-              <TextField
-                fullWidth
-                onChange={(e) => setUsername(e.target.value)}
-                label="Username"
-              ></TextField>
-              <TextField
-                fullWidth
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                label="Password"
-              ></TextField>
-            </Stack>
-          </Paper>
-        ) : (
-          <Typography>halo</Typography>
-        )}
-        <Stack direction="row" spacing={2}>
-          <Button fullWidth onClick={() => setStep((x) => x - 1)} variant="outlined">
-            Mundur
-          </Button>
-          <Button fullWidth onClick={() => setStep((x) => x + 1)} variant="contained">
-            Lanjut
-          </Button>
+        Lanjut
+      </Button>
+      <Stack spacing={1} alignItems={"center"}>
+        <Stack direction="row" spacing={2} alignItems={"center"} justifyContent={"center"}>
+          <Typography>Sudah memiliki akun?</Typography>
+          <StyledLink to="/login">
+            <Button>Masuk</Button>
+          </StyledLink>
         </Stack>
-      </Box>
+      </Stack>
     </Stack>
+  );
+}
+
+function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // undefined -> unpressed, sama kaya 0 tapi dipakai untuk transisi
+  const [step, setStep] = useState<number | undefined>(undefined);
+  const actualStep = step == undefined ? 0 : step;
+  const isStepped = step !== undefined;
+
+  const [, setLocation] = useLocation();
+
+  const { mutate: postUsers } = useUsersPost({
+    onSuccess: () => {
+      enqueueSnackbar({
+        message: <Typography>Berhasil mendaftarkan akun!</Typography>,
+        autoHideDuration: 5000,
+        variant: "success",
+      });
+      setLocation("/");
+    },
+  });
+
+  function register() {
+    postUsers({
+      user_name: username,
+      user_password: password,
+    });
+  }
+
+  return (
+    <Grid
+      container
+      minHeight={"inherit"}
+      alignItems={"center"}
+      paddingX={8}
+      columnSpacing={{ lg: 16, md: 8 }}
+      rowSpacing={2}
+    >
+      <Grid
+        size={{
+          md: 7,
+          xs: 12,
+        }}
+      >
+        <Stack alignItems={"center"} justifyContent={"center"}>
+          {actualStep === 0 ? (
+            <ImageSidebar isStepped={isStepped} />
+          ) : (
+            <RegisterSteps step={actualStep} />
+          )}
+        </Stack>
+      </Grid>
+      <Grid size={{ md: 5, xs: 12 }}>
+        <Paper
+          sx={{
+            paddingX: 4,
+            paddingY: 8,
+          }}
+        >
+          {actualStep === 0 ? (
+            <FirstStep
+              cont={() => setStep(1)}
+              password={password}
+              username={username}
+              setUsername={setUsername}
+              setPassword={setPassword}
+            />
+          ) : actualStep === 1 ? (
+            <SecondStep cont={() => setStep(2)} back={() => setStep(0)} />
+          ) : (
+            <ThirdStep cont={() => setStep(3)} back={() => setStep(1)} />
+          )}
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
