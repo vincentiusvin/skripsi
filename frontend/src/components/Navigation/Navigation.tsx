425f4209623c09bcad8e4f9b21927f3f15779016
vincentiusvin <vincentius_vin@yahoo.com>
2024-10-31 14:57:00 +0700

    I am going to hell for this

--- frontend/src/components/Navigation/Navigation.tsx
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -6,12 +6,44 @@ import { NavigationContext, NavigationData } from "./NavigationContext.ts";
 import SideNav from "./SideNav.tsx";
 import TopNav from "./TopNav.tsx";
 
-function Navigation(props: { children: ReactNode }) {
-  const { children } = props;
-  const nav = useState<NavigationData>({
+const suppress = ["/landing", "/login", "/register"];
+
+// Matiin navbar untuk halaman2 diatas.
+// Terus restore state terakhir setelah leave halaman tersebut.
+// Tapi juga bolehin user untuk buka navbar di halaman tadi, jangan dihide.
+function useLocationAwareNav() {
+  const [nav, setNav] = useState<NavigationData>({
     type: "browse",
     open: true,
   });
+  const [loc] = useLocation();
+  const [manualOverride, setManualOverride] = useState(false);
+
+  const overrideNav = { ...nav };
+  if (!manualOverride && suppress.includes(loc)) {
+    overrideNav.open = false;
+  }
+
+  useEffect(() => {
+    setManualOverride(false);
+  }, [loc]);
+
+  function overrideSetNav(arg: Parameters<typeof setNav>[0]) {
+    setManualOverride(true);
+    if (typeof arg === "function") {
+      setNav(arg(overrideNav));
+    } else {
+      setNav(arg);
+    }
+  }
+
+  return [overrideNav, overrideSetNav] as const;
+}
+
+function Navigation(props: { children: ReactNode }) {
+  const { children } = props;
+  const [nav, setNav] = useLocationAwareNav();
+
   const [loc] = useLocation();
 
   useEffect(() => {
@@ -20,7 +52,7 @@ function Navigation(props: { children: ReactNode }) {
 
   return (
     <Box>
-      <NavigationContext.Provider value={nav}>
+      <NavigationContext.Provider value={[nav, setNav]}>
         <TopNav />
         <Stack direction={"row"} mt={2} flexGrow={1}>
           <SideNav />
