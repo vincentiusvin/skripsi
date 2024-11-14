e945bd24da9190ed2477209684ddb63dd8984308
vincentiusvin <vincentius_vin@yahoo.com>
2024-11-14 10:22:50 +0700

    ok!

--- frontend/src/routes/User/FindUsersPage.tsx
+++ frontend/src/routes/User/FindUsersPage.tsx
@@ -2,14 +2,20 @@ import { SearchOutlined } from "@mui/icons-material";
 import { InputAdornment, Skeleton, Stack, TextField, Typography } from "@mui/material";
 import { useDebounce } from "use-debounce";
 import UserCard from "../../components/Cards/UserCard.tsx";
+import QueryPagination from "../../components/QueryPagination.tsx";
+import useQueryPagination from "../../components/QueryPagination/hook.ts";
 import { useStateSearch } from "../../helpers/search.ts";
 import { useUsersGet } from "../../queries/user_hooks.ts";
 
 function FindUsers() {
   const [keyword, setKeyword] = useStateSearch("keyword");
   const [debouncedKeyword] = useDebounce(keyword, 300);
+  const [page, setPage] = useQueryPagination();
+  const limit = 10;
   const { data: users_raw } = useUsersGet({
     keyword: debouncedKeyword?.toString(),
+    page,
+    limit,
   });
   const users = users_raw?.result;
 
@@ -32,6 +38,7 @@ function FindUsers() {
         }}
         onChange={(e) => {
           setKeyword(e.target.value);
+          setPage(1);
         }}
       />
       {users != undefined ? (
@@ -39,6 +46,7 @@ function FindUsers() {
       ) : (
         <Skeleton />
       )}
+      <QueryPagination limit={limit} total={users_raw?.total} />
     </Stack>
   );
 }
