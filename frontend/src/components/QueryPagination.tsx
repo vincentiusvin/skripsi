4c1f9f667bb589c4ebea3995d70b4dbdbc3a6235
vincentiusvin <vincentius_vin@yahoo.com>
2024-11-14 09:18:31 +0700

    total rows

--- frontend/src/components/QueryPagination.tsx
+++ frontend/src/components/QueryPagination.tsx
@@ -1,4 +1,4 @@
-import { Pagination } from "@mui/material";
+import { Pagination, Stack, Typography } from "@mui/material";
 import useQueryPagination from "./QueryPagination/hook.ts";
 
 function countTotalPages(total: number | undefined, limit: number) {
@@ -12,7 +12,19 @@ function QueryPagination(props: { total: number | undefined; limit: number }) {
   const { total, limit } = props;
   const [page, setPage] = useQueryPagination();
   return (
-    <Pagination count={countTotalPages(total, limit)} page={page} onChange={(_, p) => setPage(p)} />
+    <Stack alignItems={"center"} spacing={1}>
+      <Typography>Ditemukan {total} hasil</Typography>
+      <Pagination
+        showFirstButton
+        showLastButton
+        sx={{
+          margin: "auto",
+        }}
+        count={countTotalPages(total, limit)}
+        page={page}
+        onChange={(_, p) => setPage(p)}
+      />
+    </Stack>
   );
 }
 
