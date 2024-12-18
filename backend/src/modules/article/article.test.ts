596966dce2823b55ad34d09f635e5ab269e0f7d5
vincentiusvin <vincentius_vin@yahoo.com>
2024-12-18 17:16:20 +0700

    Rerun test

--- backend/src/modules/article/article.test.ts
+++ backend/src/modules/article/article.test.ts
@@ -4,7 +4,7 @@ import { baseCase } from "../../test/fixture_data.js";
 import { APIContext, getLoginCookie } from "../../test/helpers.js";
 import { clearDB } from "../../test/setup-test.js";
 
-describe.only("articles api", () => {
+describe("articles api", () => {
   let app: Application;
   let caseData: Awaited<ReturnType<typeof baseCase>>;
 
