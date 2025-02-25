351071a055fa77ba7a54de8f5171160922603d37
vincentiusvin <vincentius_vin@yahoo.com>
2025-02-25 20:50:43 +0700

    Add confirmation for tasks

--- frontend/src/routes/Projects/ProjectsKanbanPage/EditBucketDialog.tsx
+++ frontend/src/routes/Projects/ProjectsKanbanPage/EditBucketDialog.tsx
@@ -1,5 +1,6 @@
 import { MoreVert } from "@mui/icons-material";
 import {
+  Alert,
   Box,
   Button,
   Dialog,
@@ -17,20 +18,25 @@ import {
   useBucketsDetailDelete,
   useBucketsDetailGet,
   useBucketsDetailPut,
+  useTasksGet,
 } from "../../../queries/task_hooks.ts";
 
 function EditBucketDialog(props: { bucket_id: number }) {
   const { bucket_id } = props;
   const [active, setActive] = useState(false);
   const { data: bucket } = useBucketsDetailGet({ bucket_id });
+  const { data: tasks } = useTasksGet({
+    bucket_id,
+  });
   const [newName, setNewName] = useState<string | undefined>();
+  const [promptDeletion, setPromptDeletion] = useState(false);
   const { mutate: editBucket } = useBucketsDetailPut({
     bucket_id,
     onSuccess: () => {
       reset();
     },
   });
-  const { mutate: deleteBucket } = useBucketsDetailDelete({
+  const { mutate: _deleteBucket } = useBucketsDetailDelete({
     bucket_id,
     onSuccess: () => {
       enqueueSnackbar({
@@ -43,6 +49,19 @@ function EditBucketDialog(props: { bucket_id: number }) {
   function reset() {
     setNewName(undefined);
     setActive(false);
+    setPromptDeletion(false);
+  }
+
+  function deleteBucket() {
+    if (bucket == undefined || tasks == undefined) {
+      return;
+    }
+
+    if (tasks.length > 0) {
+      setPromptDeletion(true);
+    } else {
+      _deleteBucket();
+    }
   }
 
   if (!bucket) {
@@ -53,6 +72,12 @@ function EditBucketDialog(props: { bucket_id: number }) {
     <>
       <Dialog open={active} onClose={() => reset()}>
         <DialogTitle>Edit Kategori</DialogTitle>
+        {promptDeletion ? (
+          <Alert severity="warning">
+            <Typography>Yakin ingin menghapus kelompok tugas ini?</Typography>
+            <Typography>Anda memiliki {tasks?.length} tugas yang akan hilang.</Typography>
+          </Alert>
+        ) : null}
         <DialogContent>
           <Box my={1}>
             <TextField
@@ -64,13 +89,24 @@ function EditBucketDialog(props: { bucket_id: number }) {
           </Box>
         </DialogContent>
         <DialogActions>
-          <Button
-            onClick={() => {
-              deleteBucket();
-            }}
-          >
-            Hapus Kelompok Tugas
-          </Button>
+          {promptDeletion ? (
+            <Button
+              onClick={() => {
+                _deleteBucket();
+              }}
+              color="error"
+            >
+              Ya, hapus kelompok tugas
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                deleteBucket();
+              }}
+            >
+              Hapus Kelompok Tugas
+            </Button>
+          )}
           <Button
             onClick={() => {
               editBucket({
